0a6875f29d495da159549ffadd2a834e39a50456
seaflop <179542068+seaflop@users.noreply.github.com>
2026-01-14 20:32:26 -0500

    Removed an unnecessary test file

--- test.py
+++
@@ -1,9 +0,0 @@
-# Right of the player:  player.getX() + player.getWidth()
-# Left of the player:   player.getX()
-# Top of the player:    player.getY() - player.getHeight() + 2
-# Bottom of the player: player.getY() + 2
-
-# Right of the block:   blocksList[counter].getX() + blocksList[counter].getWidth()
-# Left of the block:    blocksList[counter].getX()
-# Top of the block:     blocksList[counter].getY()
-# Bottom of the block:  blocksList[counter].getY() + blocksList[counter].getHeight()
